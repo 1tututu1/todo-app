834ed4e1fc68629164ed2b7d69829b998934dec1
1tututu1 <vovafinogenov621@gmail.com>
2024-08-30 13:24:44 +0300

    add configs & migrations

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,25 @@ import (
 	"github.com/1tututu1/pkg/handler"
 	"github.com/1tututu1/pkg/repository"
 	"github.com/1tututu1/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todoapp.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running")
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
